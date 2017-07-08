fe37aeddd78890ff19ad344502a57bdaa46676b7
tamir geva <gevagt10@gmail.com>
2017-07-08 05:28:20 -0400

    commit add firebase

--- MapBlender/MapBlender/ViewController.swift
+++ MapBlender/MapBlender/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 import MapKit
+import Firebase
+import FirebaseDatabase
+
 class ViewController: UIViewController {
 //
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let ref = FIRDatabase.database().reference(withPath: "places")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
