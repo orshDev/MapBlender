1ef6271dab0116d82db3fd48ce31e37d7fa656ed
orshDev <aviorshechter@gmail.com>
2017-07-08 11:35:37 +0300

    first commit

---
+++ MapBlender/MapBlender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapBlender
+//
+//  Created by MacOS on 7/8/17.
+//  Copyright © 2017 MacOS. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
